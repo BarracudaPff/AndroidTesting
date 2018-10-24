a275352c5ba98bda65f10f0a62da93484fcbcd3e
BarracudaPff <mrm.kikll@gmail.com>
2018-10-24 14:30:25 +0300

    main act

--- app/src/main/java/com/samsung/barracudapff/androidtesting/MainActivity.java
+++ app/src/main/java/com/samsung/barracudapff/androidtesting/MainActivity.java
@@ -1,9 +1,14 @@
 package com.samsung.barracudapff.androidtesting;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,10 +16,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "Toast: " + getClass().getSimpleName() + " OnCreate", Toast.LENGTH_SHORT).show();
+
+        Button button = findViewById(R.id.button_next);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                start(MainActivity.this, TestEditTextActivity.class);
+            }
+        });
     }
 
-    public static void start(Context context, Class aClass) {
-        Intent starter = new Intent(context, aClass);
-        context.startActivity(starter);
+    public static void start(Activity activity, Class aClass) {
+        Intent starter = new Intent(activity, aClass);
+        activity.startActivity(starter);
     }
 }
